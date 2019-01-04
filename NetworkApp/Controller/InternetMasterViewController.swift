fd5ac908a234ace218e4b49fac94610f622699dd
Kaylie Hollander <Kphollander1@gmail.com>
2019-01-04 07:55:35 -0700

    noln;/

---
+++ NetworkApp/Controller/InternetMasterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InternetMasterViewController.swift
+//  NetworkApp
+//
+//  Created by Hollander, Kaylie on 1/2/19.
+//  Copyright © 2019 Hollander, Kaylie. All rights reserved.
+//
+
+import UIKit
+
+class InternetMasterViewController: UICollectionViewCell {
+    
+}
